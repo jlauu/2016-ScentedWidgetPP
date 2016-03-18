b28e45af6ae5eab8946ae7757fe20fd55314c77e
opaqe <johnny.df.lau@gmail.com>
2016-03-17 23:43:09 -0400

    Added listeners for chrome.webNavigation events. Should reconsider need for content scripts.

--- ext/sessionPage.js
+++ ext/sessionPage.js
@@ -4,17 +4,37 @@ function addUrl(url) {
     urlSequence.push(url);
 }
 
-chrome.extension.onMessage.addListener(function (msg) {
-        // Log anchor tag clicks
-        if (msg.href) {
-            console.log("log url: " + msg.href);
-            addUrl(msg.href);
-        }
+// Logs urls of web resources downloaded
+chrome.webNavigation.onCompleted.addListener(function (details) {
+    addUrl(details.url);
 });
 
+// Logs only links that content scripts modified
+/*chrome.extension.onMessage.addListener(function (msg) {*/
+/*        // Log anchor tag clicks*/
+/*        if (msg.href) {*/
+/*            console.log("log url: " + msg.href);*/
+/*            addUrl(msg.href);*/
+/*        }*/
+/*});*/
+
+// Log tab updates
 /*chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {*/
+/*    console.log("tab updated");*/
 /*    if (changeInfo) {*/
 /*        if (changeInfo.url) {*/
+/*            console.log("log url: " + changeInfo.url);*/
+/*            addUrl(changeInfo.url);*/
+/*        }*/
+/*    }*/
+/*}*/
+
+// Log tab creation
+/*chrome.tabs.onCreated.addListener(function (tabId, changeInfo, tab) {*/
+/*    console.log("tab created");*/
+/*    if (changeInfo) {*/
+/*        if (changeInfo.url) {*/
+/*            console.log("log url: " + changeInfo.url);*/
 /*            addUrl(changeInfo.url);*/
 /*        }*/
 /*    }*/
