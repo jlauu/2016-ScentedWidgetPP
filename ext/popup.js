b5ac85cbe2651859bc1bf8dd23c669fe546cf7e2
Johnny <johnny.df.lau@gmail.com>
2016-06-30 17:43:48 -0400

    Popup shows cluster of current url tab

--- ext/popup.js
+++ ext/popup.js
@@ -1,17 +1,22 @@
 'use strict';
 
+// Pull graph data from local/sync storage, else fetch from webhost
 function init() {
     chrome.storage.sync.get('swpp_graph', function (o) {
         if (o.swpp_graph) {
-            SWPP.init(o.swpp_graph);
+            MiniSWPP.init(o.swpp_graph);
         } else {
             var xhr = new XMLHttpRequest();
             var userid = chrome.extension.getBackgroundPage().session.userID;
             xhr.onreadystatechange = function () {
                 if (xhr.readyState == 4 && xhr.status == 200) {
                     var json = JSON.parse(xhr.responseText);
-                    console.log(json);
-                    SWPP.init(json);
+                    chrome.tabs.query({
+                        'active': true,
+                        'currentWindow': true
+                    }, function (tabs) {
+                        MiniSWPP.init(tabs[0].url, json);
+                    });
 /*                    chrome.storage.sync.set({'swpp_graph':json}, function () {*/
 /*                        console.log("graph stored");*/
 /*                    });*/
@@ -23,101 +28,17 @@ function init() {
     });
 }
 
-var SWPP = (function () {
+// Minimap: navigation and graph labelling/editing
+var MiniSWPP = (function () {
     var swpp = {
         width: window.innerWidth,
         height: window.innerHeight,
         svg: null,
         data: null,
+        force: null,
         links: [],
         nodes: [],
-        hover: {},
-        selected: null, // cluster id
-        force: null,
-        foci: [],
-        focus_tick: null,
-        theta: null,
-        reset_foci: null,
-        ring_clusters: [],
-        dispatch: null,
-        selected_nodes: []
     };
-    var cluster_representatives = {};
-    
-    var init_focus_tick = function () {
-        swpp.setRingFocus();
-        swpp.reset_foci = swpp.setRingFocus;
-        return ring_foci_tick;
-    };
-
-    swpp.selectCluster = function (id) {
-        swpp.selected = id;
-        swpp.dispatch.select(id);
-    };
-
-    swpp.ring_shift_left = function () {
-        swpp.force.alpha(.2);
-        if (swpp.selected == 0 || swpp.selected) {
-            swpp.ring_clusters.push(swpp.selected);
-            swpp.foci.push({x:swpp.width/2, y:swpp.height/2});
-        }
-        swpp.selectCluster(swpp.ring_clusters.shift());
-        swpp.foci.shift();
-    };
-
-    swpp.ring_shift_right = function () {
-        swpp.force.alpha(.2);
-        if (swpp.selected == 0 || swpp.selected) {
-            swpp.ring_clusters = [swpp.selected].concat(swpp.ring_clusters);
-            swpp.foci = [{x:swpp.width/2, y:swpp.height/2}].concat(swpp.foci);
-        }
-        swpp.selectCluster(swpp.ring_clusters.pop());
-        swpp.foci.pop();
-    };
-
-    swpp.setRingFocus = function () {
-        if (swpp.data) {
-            swpp.foci = [];
-            swpp.ring_clusters = [];
-            var i = 0;
-            swpp.data.groups.forEach(function (g_id) {
-                swpp.ring_clusters.push(g_id);
-                swpp.foci.push({x:0, y:0});
-            });
-            swpp.focus_tick = ring_foci_tick;
-            swpp.force.linkDistance(function (d) {
-                if (swpp.selected) {
-                    var s_id = swpp.selected.__data__.group;
-                    console.log(s_id);
-                    if (s_id == d.source.group || s_id == d.target.group) {
-                        return 50;
-                    }
-                }
-                return 15;
-            });
-            swpp.force.start();
-        }
-    };
-
-    function ring_foci_tick (theta) {
-        var r = Math.min(swpp.width, swpp.height) / 3;
-        if (theta) {
-            swpp.theta = theta;
-        } else if (!swpp.theta) {
-            swpp.theta = 0;
-        }
-        var theta = swpp.theta;
-/*        theta += Math.log(swpp.foci.length) / 2500;*/
-        var offset = 2 * Math.PI / (swpp.foci.length);
-        var a = 2 * Math.log(swpp.foci.length);
-        for (var i in swpp.foci) {
-            swpp.foci[i] = {
-                'x': a * Math.cos(theta + i*offset - Math.PI/2) * r + swpp.width/2,
-                'y': 1.2 * Math.sin(theta + i*offset - Math.PI/2) * r + swpp.height*1.3
-            }
-        }
-        swpp.theta = theta;
-    }
 
     function resize() {
         swpp.width = window.innerWidth;
@@ -129,19 +50,67 @@ var SWPP = (function () {
         swpp.force.size([swpp.width,swpp.height]).resume();
     }
 
-    swpp.init = function (json) {
-        var width = swpp.width,
-            height = swpp.height;
+    function emptyGraph() {
+        return {'nodes':[],'links':[],'groups':[]};
+    }
+
+    function eqURL(url_a, url_b) {
+        var parser = document.createElement('a');
+        parser.href = url_a;
+        url_a = parser.href;
+        parser.href = url_b;
+        url_b = parser.href;
+        return url_a == url_b;
+    }
+
+    swpp.getClusterFromUrl = function (url, graph) {
+        var node = graph.nodes.find(function (n) {
+            return eqURL(n.url, url);
+        });
+        if (!node) return emptyGraph();
+        var group_id = node.group;
+        var nodes = [];
+        graph.nodes.forEach(function (n) {
+            if (n.group == group_id) {
+                if (eqURL(url, n.url)) {
+                    n.focus = true;
+                } else {
+                    n.focus = false;
+                }
+                nodes.push(n);
+            }
+        });
+        var links = [];
+        graph.links.forEach(function (e) {
+            var sourceNode = graph.nodes.find(function (n) {
+                return n.id === e.source;
+            });
+            var targetNode = graph.nodes.find(function (n) {
+                return n.id === e.target;
+            })
+            if (sourceNode.group == group_id || targetNode.group == group_id) {
+                links.push({
+                    source: sourceNode,
+                    target: targetNode,
+                    value: e.value
+                });
+            }
+        });
+        return {'nodes':nodes, 'links':links, 'groups':[group_id]};
+    }
+
+    swpp.init = function (url, json) {
+        var width = swpp.width;
+        var height = swpp.height;
 
         var force = d3.layout.force()
             .charge(-120)
-            .gravity(0)
-            .linkDistance(30)
+            .gravity(0.3)
+            .linkDistance(15)
             .linkStrength(.2)
-            .friction(.85)
             .size([width,height])
         swpp.force = force;
-
+        
         var color = d3.scale.category20();
         var svg = d3.select("div.svg-container").append("svg")
             .attr("preserveAspectRatio", "xMinYMin meet")
@@ -153,27 +122,15 @@ var SWPP = (function () {
         });
 
         swpp.data = json;
-        var graph = json;
+        var graph = swpp.getClusterFromUrl(url, json);
         swpp.nodes = graph.nodes;
+        swpp.links = graph.links;
 
-        // Initialize links
-        graph.links.forEach(function (e) {
-            var sourceNode = graph.nodes.find(function (n) {
-                return n.id === e.source;
-            });
-            var targetNode = graph.nodes.find(function (n) {
-                return n.id === e.target;
-            });
-            swpp.links.push({
-                source: sourceNode,
-                target: targetNode,
-                value: e.value
-            });
-        });
         force
             .nodes(swpp.nodes)
             .links(swpp.links)
             .on("tick", tick)
+            .start();
 
         // build the arrow.
         svg.append("svg:defs").selectAll("marker")
@@ -187,7 +144,7 @@ var SWPP = (function () {
             .attr("markerHeight", 6)
             .attr("orient", "auto")
           .append("svg:path")
-            .attr("d", "M0,-5L10,0L0,5");
+            .attr("d", "M0, 0L0,0L0,5");
 
         // add the links and the arrows
         var path = svg.append("svg:g").selectAll("path")
@@ -197,39 +154,26 @@ var SWPP = (function () {
             .attr("class", "link")
             .attr("marker-end", "url(#end)");
 
-        // define nodes and data bindings
         var nodes = svg.selectAll(".node")
             .data(force.nodes(), function (d) {return d.id;})
           .enter().append("g")
             .attr("class", "node")
             .attr("cx", function (d) {return d.x;})
             .attr("cy", function (d) {return d.y;})
-            .style("fill", function (d) {return color(d.group); })
+            .style("fill", function (d) {
+                return color(d.focus ? 10 : 0); 
+            })
             .call(force.drag)
             .on("dblclick", function (d) {
                 var win = window.open(d.url, '_blank');
                 win.focus();
             });
-        
-        // reference to cluster representative
-        nodes.data().map(function (d,i) {
-            if (cluster_representatives[d.group]) {
-                d.rep = false;
-            } else {
-                d.rep = true;
-                cluster_representatives[d.group] = d;
-            }
-            d.y = swpp.height * 1.2;
-        });
 
-        // add the nodes
         nodes.append("circle")
-            .attr("r", 5);
+            .attr("r", function (d) {return d.focus ? 7 : 5;});
 
-        swpp.focus_tick = init_focus_tick();
-        // DEFINE ticks
+        // define nodes and data bindings
         function tick(e) {
-            swpp.focus_tick(0);
             path.attr("d", function(d) {
                 var dx = d.target.x - d.source.x,
                     dy = d.target.y - d.source.y,
@@ -241,81 +185,12 @@ var SWPP = (function () {
                     d.target.x + "," + 
                     d.target.y;
             });
-            
             nodes.attr("transform", function (d, i) {
-                var focus, k;
-                if (d.rep) {
-                    var index = swpp.ring_clusters.indexOf(d.group);
-                    if (index > -1) {
-                        focus = swpp.foci[index];
-                    // default to center
-                    } else {
-                        focus = {x: swpp.width/2, y: swpp.height/2};
-                    }
-                    k = e.alpha * 1.5;
-                } else {
-                    var rep = cluster_representatives[d.group];
-                    focus = {x:rep.x, y:rep.y};
-                    k = e.alpha * .2;
-                }
-                var x = d.x - focus.x,
-                    y = d.y - focus.y,
-                    l = Math.sqrt(x*x + y*y);
-                if (l > 1) {
-                    l = l / l * k;
-                    d.x -= x *= l;
-                    d.y -= y *= l;
-                }
                 return ["translate(",d.x,",",d.y,")"].join(" ");
             });
         }
-
-        // Hovering effects
-        d3.selectAll(".node")
-          .on("mouseover", function (d) {
-             var text = d3.select(this).append("text")
-                .attr('id', 'node' + d.id.toString())
-                .attr("x", 12)
-                .attr("dy", ".35em")
-                .text(function (d) {
-                    var t = document.createElement('a');
-                    t.href = d.url;
-                    t = t.hostname + t.pathname;
-                    t = t.split('www.')
-                    t = t.length > 1 ? t[1] : t[0];
-                    return t;
-             });
-          })
-          .on("mouseleave", function (d) {
-             var node = d3.select(this)
-             node.classed('hover', false);
-             d3.selectAll('text#node'+d.id.toString()).remove();
-          });
-
-        // Cluster Selected Events
-        swpp.dispatch = d3.dispatch('select');
-        swpp.dispatch.on('select', function (g_id) {
-            var urlbox = d3.select("#urlbox");
-            var nodes = d3.selectAll('.node')
-                .filter(function (d) {return d.group == g_id;});
-            urlbox.selectAll("li").remove()
-            console.log(nodes);
-            nodes.each(function (n) {
-                urlbox.select("ul").append("li")
-                    .text(n.url);
-            });
-        });
-
-        // Key Events
-        d3.select("body")
-            .on('keydown', function () {
-                if (d3.event.keyCode == 37) { // left
-                    swpp.ring_shift_left();
-                } else if (d3.event.keyCode == 39) { // right
-                    swpp.ring_shift_right();
-                }
-            });
     };
+
     return swpp;
 }());
 
