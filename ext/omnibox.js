de6fa0e72928809f76c699bd41b1ad7989bf5e79
Johnny Lau <johnny.lau@tufts.edu>
2016-07-13 18:11:12 -0400

    Omnibox search for clusters

---
+++ ext/omnibox.js
@@ -0,0 +1,28 @@
+//omnibox.js - event handlers for omnibox
+(function () {
+    var obx = chrome.omnibox;
+    
+    function generateDescription(cluster) {
+        var main = cluster.name;
+        var kws = Array.from(cluster.keywords);
+        var sub = kws.join(', ');
+        return main + ' - <dim>' + sub + '</dim>';
+    }
+
+    function ClusterSuggest(cluster) {
+        return {
+            'content': cluster.name,
+            'description': generateDescription(cluster)
+        };
+    }
+
+    obx.setDefaultSuggestion({
+        description: "Search for clusters"
+    });
+
+    // Return a list of matched clusters
+    obx.onInputChanged.addListener(function (text, suggest) {
+        var results = clusters.getClusters().map(ClusterSuggest);
+        suggest(results);
+    });
+})();
